f4cf51499d74c3b61e0856b2bf2b8a14b876be60
Dennys Humberto <42494584+dennyshbr@users.noreply.github.com>
2025-05-26 11:44:10 -0300

    Add ; com erro

--- src/Controllers/WeatherForecastController.cs
+++ src/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ namespace Api_Pipeline.Controllers
 
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : ControllerBase;
     
         private static readonly string[] Summaries = new[]
         {
