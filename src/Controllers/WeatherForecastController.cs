1ca7a0439834c9e55ea7fa6eb5444cd0ffa2fde5
Dennys Humberto <42494584+dennyshbr@users.noreply.github.com>
2025-05-26 11:40:40 -0300

    Quebrando a classe

--- src/Controllers/WeatherForecastController.cs
+++ src/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ namespace Api_Pipeline.Controllers
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
-    {
+    
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
